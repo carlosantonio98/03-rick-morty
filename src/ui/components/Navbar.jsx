c2c28e5efd0aa8c817bb302bec1cbe7a6cbe78fb
carloscamacho <carlosalvarez9805@gmail.com>
2023-03-17 17:11:31 -0600

    Add responsive navbar and pagination style

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,12 +1,20 @@
 import { NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
+
+    const showResponsiveMenu = () => {
+        const navMenu = document.querySelector('.nav-menu');
+        navMenu.classList.toggle('show');
+    }
+
     return (
-        <nav className='w-[80%] mx-auto mb-10 py-4 animate__animated animate__fadeIn'>
-            <ul className="flex">
-                <li className="mr-14 font-bold text-green-500">
-                    <NavLink to="characters">Rick<span className="text-black text-xs">&</span>Morty</NavLink>
-                </li>
+        <nav className='relative z-[100] flex w-[80%] mx-auto mb-10 py-4 animate__animated animate__fadeIn'>
+
+            <div className='mr-14 font-bold text-green-500'>
+                <NavLink to="characters">Rick<span className="text-black text-xs">&</span>Morty</NavLink>
+            </div>
+            
+            <ul className="flex nav-menu">
                 <li className="mr-4">
                     <NavLink to="characters" className={ ({ isActive }) => isActive ? 'border-b-2 border-green-500' : '' }>Characters</NavLink>
                 </li>
@@ -19,10 +27,12 @@ export const Navbar = () => {
                 <li className="mr-4">
                     <NavLink to="search" className={ ({ isActive }) => isActive ? 'border-b-2 border-green-500' : '' }>Search</NavLink>
                 </li>
-                {/* <li className='ml-auto'>
-                    <NavLink to="login" className={ ({ isActive }) => isActive ? 'border-b-2 border-green-500' : '' }>Logout</NavLink>
-                </li> */}
             </ul>
+            
+            <div className='relative z-[100] ml-auto nav-toggle' onClick={ showResponsiveMenu }>
+                <i className="fa-solid fa-bars"></i>
+            </div>
+
         </nav>
     );
 }
